92ea4fceaee39cc0bbb2493849b63a15b64f2235
Juhana Leinonen <git@nitku.net>
2020-10-22 12:13:03 +0300

    Basic TS/SASS/Webpack bootstrapping

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a basic setup for Typescript + SASS + Webpack. 
+
+* All files in the assets directory will be copied to the build directory. 
+* JS and CSS files app.[version].js and styles.[version].js are created with [version] corresponding to the version number in package.json
+* The text `{{version}}` in HTML asset files will be replaced with the version number
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require( "path" );
+const CopyPlugin = require( "copy-webpack-plugin" );
+const { CleanWebpackPlugin } = require( "clean-webpack-plugin" );
+const Dotenv = require( "dotenv-webpack" );
+
+const APP_VERSION = require( "./package.json" ).version;
+
+module.exports = ( _, argv ) => {
+    const isProduction = argv.mode === "production";
+
+    return {
+        entry: [
+            "./src/index.ts",
+            "./src/styles.scss"
+        ],
+        devtool: "source-map",
+        module: {
+            rules: [
+                // Typescript files
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                },
+                // SASS style files
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates CSS files
+                        {
+                            loader: "file-loader",
+                            options: { name: `[name].${APP_VERSION}.css` }
+                        },
+                        // Compiles SASS to CSS
+                        "sass-loader"
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: [ ".tsx", ".ts", ".js" ]
+        },
+        output: {
+            filename: `app.${APP_VERSION}.js`,
+            path: path.resolve( __dirname, "build" )
+        },
+        devServer: {
+            contentBase: path.join( __dirname, "assets" ),
+            compress: true,
+            port: 3011
+        },
+        plugins: [
+            // Clear the build directory
+            new CleanWebpackPlugin(),
+
+            // Include assets
+            new CopyPlugin({
+                patterns: [
+                    {
+                        context: "./assets",
+                        from: "**/*",
+                        transform: ( content, path ) => {
+                        // insert app version to templates
+                            if( path.includes( ".html" ) ) {
+                                console.log( path, content );
+                                return Buffer.from( content.toString().split( "{{version}}" ).join( APP_VERSION ) );
+                            }
+
+                            return content;
+                        }
+                    }
+                ]
+            }),
+
+            // Read environment variables
+            new Dotenv({
+                path: isProduction ? ".env.production" : ".env"
+            })
+        ]
+    };
+};
